de971daa15569b92d7acfcd6094683299ce9215e
Barto <bartwietecki@gmail.com>
2023-08-03 17:03:31 +0200

    feature/make-order: improved view for summary.html

--- src/main/java/com/onlinebookstore/model/OrderModel.java
+++ src/main/java/com/onlinebookstore/model/OrderModel.java
@@ -1,12 +1,12 @@
 package com.onlinebookstore.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 public class OrderModel {
-
     private Long id;
     private String customerFullName;
     private String customerEmail;
