9ee5f4e156d2c90d9799070aece33b70de3abcc6
Barto <bartwietecki@gmail.com>
2023-08-05 11:18:35 +0200

    feature/register-and-login: implementing the User and Role repositories

---
+++ src/main/java/com/onlinebookstore/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.onlinebookstore.repository;
+
+import com.onlinebookstore.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByUsername(String username);
+}
