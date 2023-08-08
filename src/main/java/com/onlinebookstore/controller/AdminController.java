fa0e23c11b46bb4d67b6815325f9e785d949c3d1
Barto <bartwietecki@gmail.com>
2023-08-08 19:50:05 +0200

    flyway implementation to the project

--- src/main/java/com/onlinebookstore/controller/AdminController.java
+++ src/main/java/com/onlinebookstore/controller/AdminController.java
@@ -1,8 +1,6 @@
 package com.onlinebookstore.controller;
 
-import com.onlinebookstore.config.AdminConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,24 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final AdminConfiguration adminConfiguration;
-
-    public AdminController(AdminConfiguration adminConfiguration) {
-        this.adminConfiguration = adminConfiguration;
-    }
-
-    //    @GetMapping
-//    public String admin(Model model) {
-//        return "admin-home";
-//    }
-
-    // method change because of , @Configuration Properites
     @GetMapping
-    public String admin(Model model) {
-        String username = adminConfiguration.getUsername();
-        String password = adminConfiguration.getPassword();
-        model.addAttribute("adminUsername", username);
-        model.addAttribute("adminPassword", password);
+    public String admin() {
         return "admin-home";
     }
 }
